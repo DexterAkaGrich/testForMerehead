7e6f64abb7b2b77c323b6d0f9bcc2829d6335b82
Dmitry <veni4ek18@gmail.com>
2019-01-12 01:29:18 +0200

    styles

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ function init (result) {
     if(result.users){
     const names = result.users.map((item, index) => `${index + 1}) ${item.name} ${item.surname}`)
     console.log(names);
+                                                  //css
+    let buttonStyle = {
+      background: '#caddda',
+      padding: '3px 7px',
+      color: '#494949',
+      border: '1px solid #666',
+      'font-size': '1.2em'
+    }
+    let liStyle = {
+      background: '#caddda',
+      padding: '3px 7px',
+      color: '#494949',
+      'list-style-type': 'none',
+      'font-size': '1.2em'
+    }
 
     //Пагинатор
     class TodoApp extends React.Component {
@@ -33,7 +48,7 @@ function init (result) {
         const currentTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo);
 
         const renderTodos = currentTodos.map((todo, index) => {
-          return <li key={index}>{todo}</li>;
+          return <li style={liStyle} key={index}>{todo}</li>;
         });
 
         const pageNumbers = [];
@@ -43,13 +58,14 @@ function init (result) {
 
         const renderPageNumbers = pageNumbers.map(number => {
           return (
-            <li
+            <button
+              style = {buttonStyle}
               key={number}
               id={number}
               onClick={this.handleClick}
             >
               {number}
-            </li>
+            </button>
           );
         });
 
